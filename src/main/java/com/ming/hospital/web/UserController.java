f18e06fe68efef2811442d7dd891d7237405edbd
langlibaihu <41273839+langlibaihu@users.noreply.github.com>
2020-04-28 14:19:10 +0800

    啊实打实

--- src/main/java/com/ming/hospital/web/UserController.java
+++ src/main/java/com/ming/hospital/web/UserController.java
@@ -27,61 +27,61 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/register",method = {RequestMethod.POST})
-    public String register(User user,Model model) {
+    @RequestMapping(value = "/register", method = {RequestMethod.POST})
+    public String register(User user, Model model) {
         Date date = new Date();
         user.setCreatetime(date);
         user.setUpdatetime(date);
         user.setUid(CommonUtils.getId());
-        user.setCode(UUID.randomUUID().toString().replace("-",""));//激活码
+        user.setCode(UUID.randomUUID().toString().replace("-", ""));//激活码
         boolean register = userService.register(user);
-        if(register){
+        if (register) {
             return "registerSuccess";
         }
-        model.addAttribute("msg","不好意思！注册失败！请重新注册，");
+        model.addAttribute("msg", "不好意思！注册失败！请重新注册，");
         return "error";
     }
+
     @RequestMapping("/active/{code}")
-    public String active(@PathVariable String code,Model model){
-        if(code != null){
+    public String active(@PathVariable String code, Model model) {
+        if (code != null) {
             boolean active = userService.active(code);
-            if(active){
+            if (active) {
                 return "redirect:/";
-            }else {
-                model.addAttribute("msg","激活邮箱失败！您的激活码可能超过48小时！请重新注册！");
+            } else {
+                model.addAttribute("msg", "激活邮箱失败！您的激活码可能超过48小时！请重新注册！");
             }
         }
         return "error";
     }
 
-    @RequestMapping(value = "/login",method = {RequestMethod.POST})
-    public String login(User user,HttpServletRequest request){
-        User fullUser =  userService.login(user);
-        if(fullUser != null){
+    @RequestMapping(value = "/login", method = {RequestMethod.POST})
+    public String login(User user, HttpServletRequest request) {
+        User fullUser = userService.login(user);
+        if (fullUser != null) {
             HttpSession session = request.getSession();
-            session.setAttribute("user",fullUser);
+            session.setAttribute("user", fullUser);
             return "forward:/";
         }
-        request.setAttribute("msg","账号或密码错误！请重新登陆<a href = ''>医者天下</a>");
+        request.setAttribute("msg", "账号或密码错误！请重新登陆<a href = ''>医者天下</a>");
         return "error";
     }
+
     @RequestMapping("/checkUserName")
     @ResponseBody
-    public Map checkUserName(String user){
+    public Map checkUserName(String user) {
         Map map = new HashMap();
         boolean isExist = userService.checkUserName(user);
-        map.put("isExist",isExist ? true:false);
+        map.put("isExist", isExist ? true : false);
         return map;
     }
+
     @RequestMapping("/logout")
-    public String logout(HttpServletRequest request){
+    public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.invalidate();
         return "redirect:/";
     }
-
-
-
 
 
 }
